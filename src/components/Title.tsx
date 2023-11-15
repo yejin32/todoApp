7eee396ae8cb46f922d9653cb8edf3a13d681ec6
yejin32 <yejin@yejin-ui-MacBookPro.local>
2023-11-15 17:46:53 +0900

    Feat: Title 레이아웃 구현

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 const Title = () => {
-  return <div></div>;
+  const STitleForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+
+    .header {
+      font-size: 2rem;
+      font-weight: 600;
+      margin-bottom: 5px;
+    }
+  `;
+  return (
+    <STitleForm>
+      <div className="header">TodoApp</div>
+    </STitleForm>
+  );
 };
 
 export default Title;
