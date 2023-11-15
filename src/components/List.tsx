02182d637b25ca865b17d7a6856a0fbb99d216d4
yejin32 <yejin@yejin-ui-MacBookPro.local>
2023-11-15 17:47:04 +0900

    Feat: List 레이아웃 구현

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 
 const List = () => {
-  return <div></div>;
+  const SListForm = styled.div`
+    display: flex;
+    align-items: center;
+    width: 500px;
+    height: 500px;
+    max-height: 500px;
+    border: 2px solid #ffffff;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    padding: 20px;
+
+    .noList {
+      width: 100%;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      color: #000000;
+    }
+  `;
+  return (
+    <SListForm>
+      <div className="noList">할 일이 없습니다. 자유롭게 작성해보세요!</div>
+    </SListForm>
+  );
 };
 
 export default List;
